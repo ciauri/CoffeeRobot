92f9f13f2f2600faeea5a8abb9432496e9e33470
tetsu100 <tetsu100@mail.chapman.edu>
2016-05-09 15:08:22 -0700

    Update CoffeeDetection.py

--- CoffeeDetection.py
+++ CoffeeDetection.py
@@ -25,15 +25,15 @@ def detectCoffee():
     eye_cascade = cv2.CascadeClassifier('/home/pi/Documents/OpenCV_Projects/XML_Files/liquid.xml')
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+    rgb_val = 0
     faces = face_cascade.detectMultiScale(gray, 1.2, 200, minSize=(30,50))
     for (x,y,w,h) in faces:
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 10, minSize=(25,25))
-        rgb_val = 0;
-        numPots = 0;
+        rgb_val = 0
+        numPots = 0
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
             roi_liquid_color = img[ey:ey+eh, ex:ex+ew]
@@ -44,24 +44,39 @@ def detectCoffee():
             numPots += 1
         rgb_val /= numPots
         print(rgb_val)
-    cv2.imshow('img',img) 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     return rgb_val
     
 def getAVGcoffee(numPics):
     if(numPics > 0):
         avg_rgb_val = 0
+        good_vals = 0
         for i in range(numPics):
-            avg_rgb_val += detectCoffee()
+            next_rgb_val = 0
+            next_rgb_val = detectCoffee()
+            if(next_rgb_val > 0):
+                avg_rgb_val += next_rgb_val
+                good_vals += 1
             time.sleep(1)
-        avg_rgb_val /= numPics
+        avg_rgb_val /= good_vals
         return avg_rgb_val
     
 #Main Prog Loop
+loops = 0
+constant_empty_rgb_val = 0
 while(True):
-    print("Checking for coffee... ")
-    currentCoffeeLevel = getAVGcoffee(4)
-    print("RGB val of pot is: " + currentCoffeeLevel)
-    time.sleep(3)
-
+    if(loops < 2):
+        if(loops == 0):
+            print("Calibrating, please ensure pot is EMPTY,")
+            raw_input("Press ENTER when pot is EMPTY...")
+            constant_empty_rgb_val += getAVGcoffee(4)
+        else:
+            constant_empty_rgb_val += getAVGcoffee(4)
+            constant_empty_rgb_val /= 2
+    else:
+        print("Checking for coffee... ")
+        currentCoffeeLevel = getAVGcoffee(4)
+        percentDiff = (abs(constant_empty_rgb_val - currentCoffeeLevel)/float(constant_empty_rgb_val))*100.0
+        print("RGB val of pot is: " + str(currentCoffeeLevel)))
+        print("There is approximately %d% coffee in the pot"(percentDiff))
+        time.sleep(3)
+    loops += 1
